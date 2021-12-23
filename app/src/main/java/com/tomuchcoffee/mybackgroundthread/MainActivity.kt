0bc71d3f7e9aa740ae425cdce565e95a05c138bd
mifune96 <imrank.sensea@gmail.com>
2021-12-23 09:22:46 +0700

    finish

---
+++ app/src/main/java/com/tomuchcoffee/mybackgroundthread/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.tomuchcoffee.mybackgroundthread
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.concurrent.Executors
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonStart = findViewById<Button>(R.id.btn_start)
+        val tvstatus = findViewById<TextView>(R.id.tv_status)
+
+        buttonStart.setOnClickListener {
+            lifecycleScope.launch(Dispatchers.Default){
+                for (i in 0..10){
+                    delay(500)
+                    val percentage = i*10
+                    withContext(Dispatchers.Main){
+                        //update ui main thread
+                        if (percentage == 100){
+                            tvstatus.setText(R.string.task_completed)
+                        } else{
+                            tvstatus.text = String.format(getString(R.string.compressing), percentage)
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
